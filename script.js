c8ce13b71cb8d1e443477e141b2081b918891fea
Rodrigo Viveros <rodriviveros100@gmail.com>
2026-06-18 11:18:43 -0300

    CAMBIOS EN EL JS
    ·SE AGREGO GALLERIA ACTUAIZADA DE IMAGENES
    ·SE AGREGO LA LIBRERIA SWEETALERT2
    ·SE IMPLEMENTO UNA FUNCION DE ESTA ULTMA EN EL JAVASCRIPT

--- script.js
+++ script.js
@@ -1,16 +1,37 @@
-async function cargarProductos() {
-// Cargar el archivo JSON
-try{
-const response = await fetch('product.json');
-const data = await response.json();
-console.log(data);
-const gallery=document.querySelector('.gallery');
-gallery.innerHTML='';
-data.productos.forEach(pizza=>{
-const li=document.createElement('li');
-li.innerHTML=` <div class="box">
+function abrirModal(pizza) {
+
+    Swal.fire({
+
+        title: pizza.nombre,
+        html: ` <img src="${pizza.imagen}"
+                         alt="${pizza.nombre}"
+                         style="width:100%; border-radius:8px; margin-bottom:12px">
+                    <p style="color:#555; font-size:1rem; margin-bottom:12px">
+                        ${pizza.descripcion}
+                    </p>
+                    <p style="color:#135D76; font-size:2rem; font-weight:bold; margin:0">
+                        $ ${pizza.precio.toLocaleString('es-AR')}
+                    </p>  `,
+        confirmButtonText: 'Cerrar',
+        confirmButtonColor: '#0D7139',
+        width: '500px'
+
+    });
+}
+
+    async function cargarProductos() {
+        // Cargar el archivo JSON
+        try {
+            const response = await fetch('product.json');
+            const data = await response.json();
+            console.log(data);
+            const gallery = document.querySelector('.gallery');
+            gallery.innerHTML = '';
+            data.productos.forEach(pizza => {
+                const li = document.createElement('li');
+                li.innerHTML = ` <div class="box">
                     <figure>
-                        <img src="${pizza.imagen}" alt="${pizza.nombre}"/>
+                        <img class="img-clickeable" src="${pizza.imagen}" alt="${pizza.nombre}"/>
                         <figcaption>
                             <h3>${pizza.nombre}</h3>
                             <p>$ ${pizza.precio.toLocaleString('es-AR')}</p>
@@ -21,13 +42,14 @@ li.innerHTML=` <div class="box">
                         Añadir al carrito <i class="fa-solid fa-cart-shopping"></i>
                     </button>
                 </div>`;
+                const img = li.querySelector('.img-clickeable');
+                img.addEventListener('click', () => abrirModal(pizza));
+                gallery.appendChild(li);
+            }); // End of forEach
 
-gallery.appendChild(li);
-}); // End of forEach
-
-} // End of try
-catch(error){
-console.error('Error al cargar productos:', error);                                                                                                                                                                                                                                
-}  // End of catch
-} // End of Function
-document.addEventListener('DOMContentLoaded', cargarProductos);
+        } // End of try
+        catch (error) {
+            console.error('Error al cargar productos:', error);
+        }  // End of catch
+    } // End of Function
+    document.addEventListener('DOMContentLoaded', cargarProductos);
